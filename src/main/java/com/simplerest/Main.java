5943588060ebe21810f74b4f279e5cb19a16b305
Binar Nugroho <binarnugroho775@gmail.com>
2025-12-25 15:32:01 +0700

    Rename release artifact

--- src/main/java/com/simplerest/Main.java
+++ src/main/java/com/simplerest/Main.java
@@ -4,67 +4,24 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.simplerest.HTTPClient.*;
-import okhttp3.*;
 import tools.jackson.databind.ObjectMapper;
 
 public class Main {
 
   private static final String BASE_URL = "https://api.restful-api.dev";
-  private static final MediaType JSON = MediaType.get("application/json");
   private static final ObjectMapper mapper = new ObjectMapper();
 
   public static void main(String[] args) throws Exception {
-    var builder = new OkHttpClient.Builder();
-    var client = builder.build();
-
-    var reqGet = new Request.Builder()
-      .url(
-        String.format(
-          "%s/%s",
-          BASE_URL,
-          "objects/ff8081819782e69e019b4e920c9c66b0"
-        )
-      )
-      .build();
-
-    try (var res = client.newCall(reqGet).execute()) {
-      String strBody = res.body().string();
-
-      Product p1 = mapper.readValue(strBody, Product.class);
-
-      System.out.println("Product ID: " + p1.id);
-      System.out.println("Product Name: " + p1.name);
-      System.out.println("Product Color: " + p1.data.color);
-      System.out.println("Product Capacity: " + p1.data.capacity);
-    }
-
-    // POST
-    var p2 = new Product();
-    p2.data = new ProductData();
-    p2.name = "Ipong 77";
-    p2.data.color = "red";
-    p2.data.capacity = "128";
-    p2.data.cpuModel = "Intel Core i15";
-
-    var body = RequestBody.create(mapper.writeValueAsBytes(p2), JSON);
-    var reqPost = new Request.Builder()
-      .url(
-        String.format(
-          "%s/%s",
-          BASE_URL,
-          "objects/ff8081819782e69e019b4e920c9c66b0"
-        )
-      )
-      .patch(body)
-      .build();
-
-    try (var res = client.newCall(reqPost).execute()) {
-      String strBody = res.body().string();
-      System.out.println(strBody);
-    }
-
     var b = ClientBuilder.builder();
     b.baseUrl(BASE_URL).build();
+
+    var p2 = new Product();
+    p2.name = "Ipong 99";
+    p2.data = new ProductData();
+    p2.data.cpuModel = "Intel Core i7";
+    p2.data.hardDiskSize = "1TB";
+    p2.data.color = "Black";
+    p2.data.capacity = "256GB";
 
     var httpClient = new Client(b);
     var obj = httpClient.post(
